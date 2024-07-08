b11f6611d6744e11433b613db96a4bb25f32af01
Cath3876 <schca764@gmail.com>
2024-07-08 10:04:03 +0200

    apply style changes to pysical cells

--- src/utils/styleHandlers.js
+++ src/utils/styleHandlers.js
@@ -3,21 +3,28 @@ import { TextStyleAction, CellStyleAction, ClearFormattingAction} from '../Custo
 
 export const handleStyleChange = (styleType, value, selectedCellsRef, setTextStyles, hotRef) => {
   const changes = [];
+  const hotInstance = hotRef.current.hotInstance;
+
   setTextStyles((prev) => {
     const newTextStyles = { ...prev };
     selectedCellsRef.current.forEach(([row, col]) => {
-      if (!newTextStyles[`${row}-${col}`]) {
-        newTextStyles[`${row}-${col}`] = {};
+      const visualRowIndex = hotInstance.toPhysicalRow(row);
+      const visualColIndex = hotInstance.toPhysicalColumn(col);
+      const cellKey = `${visualRowIndex}-${visualColIndex}`;
+      
+      if (!newTextStyles[cellKey]) {
+        newTextStyles[cellKey] = {};
       }
-      const oldStyle = { ...newTextStyles[`${row}-${col}`] };
+      const oldStyle = { ...newTextStyles[cellKey] };
+      
       if (styleType === 'clear formatting') {
-        newTextStyles[`${row}-${col}`] = {};
+        newTextStyles[cellKey] = {};
       } else if (styleType === 'bold') {
-        newTextStyles[`${row}-${col}`].fontWeight = newTextStyles[`${row}-${col}`].fontWeight === 'bold' ? 'normal' : 'bold';
+        newTextStyles[cellKey].fontWeight = newTextStyles[cellKey].fontWeight === 'bold' ? 'normal' : 'bold';
       } else if (styleType === 'italic') {
-        newTextStyles[`${row}-${col}`].fontStyle = newTextStyles[`${row}-${col}`].fontStyle === 'italic' ? 'normal' : 'italic';
+        newTextStyles[cellKey].fontStyle = newTextStyles[cellKey].fontStyle === 'italic' ? 'normal' : 'italic';
       } else if (styleType === 'strikethrough') {
-        newTextStyles[`${row}-${col}`].textDecoration = newTextStyles[`${row}-${col}`].textDecoration === 'line-through' ? 'none' : 'line-through';
+        newTextStyles[cellKey].textDecoration = newTextStyles[cellKey].textDecoration === 'line-through' ? 'none' : 'line-through';
       } else if (styleType === 'borderColor') {
         const minRow = Math.min(...selectedCellsRef.current.map(([row, _]) => row));
         const maxRow = Math.max(...selectedCellsRef.current.map(([row, _]) => row));
@@ -25,22 +32,22 @@ export const handleStyleChange = (styleType, value, selectedCellsRef, setTextSty
         const maxCol = Math.max(...selectedCellsRef.current.map(([_, col]) => col));
 
         if (row === minRow) {
-          newTextStyles[`${row}-${col}`].borderTop = `2px solid ${value}`;
+          newTextStyles[cellKey].borderTop = `2px solid ${value}`;
         }
         if (row === maxRow) {
-          newTextStyles[`${row}-${col}`].borderBottom = `2px solid ${value}`;
+          newTextStyles[cellKey].borderBottom = `2px solid ${value}`;
         }
         if (col === minCol) {
-          newTextStyles[`${row}-${col}`].borderLeft = `2px solid ${value}`;
+          newTextStyles[cellKey].borderLeft = `2px solid ${value}`;
         }
         if (col === maxCol) {
-          newTextStyles[`${row}-${col}`].borderRight = `2px solid ${value}`;
+          newTextStyles[cellKey].borderRight = `2px solid ${value}`;
         }
       } else {
-        newTextStyles[`${row}-${col}`][styleType] = value;
+        newTextStyles[cellKey][styleType] = value;
       }
-      changes.push({ row, col, oldStyle, newStyle: { ...newTextStyles[`${row}-${col}`] } });
-      console.log(`Modified cell [${row}, ${col}] from style:`, oldStyle, "to style:", newTextStyles[`${row}-${col}`]);
+      
+      changes.push({ row: visualRowIndex, col: visualColIndex, oldStyle, newStyle: { ...newTextStyles[cellKey] } });
     });
     return newTextStyles;
   });
@@ -51,13 +58,16 @@ export const handleStyleChange = (styleType, value, selectedCellsRef, setTextSty
     ? new CellStyleAction(changes)
     : new TextStyleAction(changes);
 
-  hotRef.current.hotInstance.undoRedo.done(() => action);
+  hotInstance.undoRedo.done(() => action);
 };
 
 export const customRenderer = (instance, td, row, col, prop, value, cellProperties, textStyles) => {
   textRenderer.apply(this, [instance, td, row, col, prop, value, cellProperties]);
-  const cellKey = `${row}-${col}`;
+  const visualRowIndex = instance.toPhysicalRow(row);
+  const visualColIndex = instance.toPhysicalColumn(col);
+  const cellKey = `${visualRowIndex}-${visualColIndex}`;
   const styles = textStyles[cellKey] || {};
+
   td.style.color = styles.color || 'black';
   td.style.backgroundColor = styles.backgroundColor || 'white';
   td.style.fontWeight = styles.fontWeight || 'normal';
